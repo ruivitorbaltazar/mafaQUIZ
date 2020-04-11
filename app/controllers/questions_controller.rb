ad5a314879d085fb072d613e4c1f8214cfc6480b
Rui Baltazar <ruivrb@gmail.com>
2020-04-11 02:41:56 +0100

    more pundits

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,13 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.new(question_params)
     fetch_quiz
-    @question.quiz =
+    @question.quiz = @quiz
+    authorize @question
+    if @question.save
+
+    else
+      render 'quiz/show'
+    end
   end
 
   def update
@@ -12,6 +18,11 @@ class QuestionsController < ApplicationController
   end
 
   private
+
+  def fetch_quiz
+    @quiz = Quiz.find(params[:id])
+    authorize @quiz
+  end
 
   def question_params
     params.require(:question).permit(:content, :correct_answer, :difficulty)
